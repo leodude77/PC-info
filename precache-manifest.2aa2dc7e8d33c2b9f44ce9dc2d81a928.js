5441b607fa327aed2b2e1963b5f7466a3f158e4a
Malli <mallikarjun.1rn17is104@gmail.com>
2020-12-18 10:32:31 +0530

    Updates

---
+++ precache-manifest.2aa2dc7e8d33c2b9f44ce9dc2d81a928.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5376512ef8a71983104385b6fce6012d",
+    "url": "/PC-info/index.html"
+  },
+  {
+    "revision": "69d572987b5f6c70ef8a",
+    "url": "/PC-info/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "3c7eb70b9e14594dfd5e",
+    "url": "/PC-info/static/css/main.4e12de2b.chunk.css"
+  },
+  {
+    "revision": "69d572987b5f6c70ef8a",
+    "url": "/PC-info/static/js/2.38180ac4.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/PC-info/static/js/2.38180ac4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c7eb70b9e14594dfd5e",
+    "url": "/PC-info/static/js/main.f70668ef.chunk.js"
+  },
+  {
+    "revision": "ba4a08a592f422a5218d",
+    "url": "/PC-info/static/js/runtime-main.682ef8b7.js"
+  },
+  {
+    "revision": "03eb9289caad339ccd7fc3eb0c7a51e6",
+    "url": "/PC-info/static/media/fan.03eb9289.png"
+  }
+]);
