49265f6df79bc7e78bc727219aa82481da5830b9
Mallikarjun <mallikarjun.1rn17is104@gmail.com>
2020-12-21 20:12:13 +0530

    fix maybe

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Main from "./Components/Main";
 
 class App extends Component {
   render() {
     return (
       <div className="App" style={{ backgroundColor: "#121212" }}>
-        <BrowserRouter>
+        <HashRouter>
           <Main />
-        </BrowserRouter>
+        </HashRouter>
       </div>
     );
   }
